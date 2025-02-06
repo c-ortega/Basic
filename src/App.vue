76b26f84303d4195dd7a20d0ca49022ee2762bbd
c-ortega <christopher.ortega@bsu.edu>
2025-02-06 14:19:34 -0500

    Init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import OrderList from "./components/OrderList.vue";
+</script>
+
+<template>
+  <OrderList />
+</template>
+
+<style scoped></style>
---
+++ src/components/OrderList.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from "vue";
+
+const items = ref([
+  { name: "Cucumber", count: 0 },
+  { name: "Carrot", count: 0 },
+  { name: "Corn", count: 100 },
+]);
+
+function increment(index) {
+  items.value[index].count++;
+}
+
+function decrement(index) {
+  if (items.value[index].count > 0) {
+    items.value[index].count--;
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Order Items</h1>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item.name }}
+        <button @click="decrement(index)">-</button>
+        {{ item.count }}
+        <button @click="increment(index)">+</button>
+      </li>
+    </ul>
+  </div>
+</template>
